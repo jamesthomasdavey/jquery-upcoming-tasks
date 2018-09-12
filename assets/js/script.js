24fc6ea732ff2a64795444f444e37d5a7096a734
James Davey <james.t.davey@gmail.com>
2018-09-12 10:35:43 -0700

    finish basic functionality for todo list

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "li span", function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    let todoText = $(this).val();
+    $("ul").append(`
+      <li><span>X</span> ${todoText}</li>
+    `);
+    $(this).val("");
+  }
+});
